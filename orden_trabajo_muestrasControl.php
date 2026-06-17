8859beedb24fbcdf1fd7b3728464aa59a01732df
RafaelRafael Flores Moreno <flores.freak@gmail.com>
2026-06-17 16:27:48 -0700

    code changes v3

--- orden_trabajo_muestrasControl.php
+++ orden_trabajo_muestrasControl.php
@@ -3,8 +3,8 @@
 	<head>
 	</head>
 		<body>		
-            <?include('partials/header.php'); ?>
-            <?include('partials/orden_trabajo_muestrasControl.php');?>
+            <?php include('partials/header.php'); ?>
+            <?php include('partials/orden_trabajo_muestrasControl.php');?>
             <?//include('partials/footer.php'); ?>
 		</body>
 	</html>
